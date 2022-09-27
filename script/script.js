26fcf43fa22159a2bddd177cc52042d12b8b8002
Gabriel Dantas <dantasbiell20@gmail.com>
2022-09-27 19:24:48 -0300

    get edit

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
 
     $('#quem-somos').on('click', () => {
         $('#quem-somos').addClass('link-ativo');
-        $.get('capas/capa-quem-somos.html', data => {
+        $.get('./capas/capa-quem-somos.html', data => {
             $('#capa').html(data)
         })
         $.get('../corpos/quem-somos.html', data => {
