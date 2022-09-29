6efd381c0c51776704ba11b0cae37fb33085d72f
Gabriel Dantas <dantasbiell20@gmail.com>
2022-09-29 19:35:14 -0300

    ajustes

--- script/script.js
+++ script/script.js
@@ -183,14 +183,4 @@ $(document).ready(() => {
             `
         }
     })
-
-    $('.slider').slick({
-        dots: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 500,
-        variableWidth: true,
-        arrows: false
-    });
 })
